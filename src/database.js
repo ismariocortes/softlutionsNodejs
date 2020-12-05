eb9a5802808f55ff4c2bc5294ee21c76a1c907d9
Mario Cortés <67335786+ismariocortes@users.noreply.github.com>
2020-12-04 18:42:36 -0600

    Update database.js

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 const mongoose =  require('mongoose');
 
 // URI para la conexión a la base de datos
-const MONGODB_URI = process.env.MONGODB_URI;
+const MONGODB_URI = 'mongodb+srv://mongoUser:tpW5LmJbN283EzzE@cluster0.bx0cv.mongodb.net/softlutions?retryWrites=true&w=majority';
 
 // Conexión a la base de datos 
 mongoose.connect(MONGODB_URI, { 
